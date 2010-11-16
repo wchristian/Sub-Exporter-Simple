34a123e0b6595eb32730756171fa9741bb2dce17
Christian Walde <walde.christian@googlemail.com>
2010-11-16 22:40:14 +0100

    Build results of dd17bee (on master)

--- lib/Sub/Exporter/Simple.pm
+++ lib/Sub/Exporter/Simple.pm
@@ -2,6 +2,11 @@ use strict;
 use warnings;
 
 package Sub::Exporter::Simple;
+BEGIN {
+  $Sub::Exporter::Simple::VERSION = '1.103200';
+}
+
+# ABSTRACT: just export some subs
 
 use Sub::Exporter 'setup_exporter';
 
@@ -10,3 +15,29 @@ sub import {
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Sub::Exporter::Simple - just export some subs
+
+=head1 VERSION
+
+version 1.103200
+
+=head1 AUTHOR
+
+Christian Walde <mithaldu@yahoo.de>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2010 by Christian Walde.
+
+This is free software, licensed under:
+
+  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE, Version 2, December 2004
+
+=cut
+
